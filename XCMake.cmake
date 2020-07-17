df3c0df5998fa3b01b37208c9354966d74710e8f
Jeffrey Xiang <junx93002@sina.com>
2020-07-17 22:12:23 +0800

    Add projectInit() and QtInit()

--- XCMake.cmake
+++ XCMake.cmake
@@ -1,3 +1,4 @@
+# Print the content of a variable with title and prefix
 function(Xi_printList)
 	cmake_parse_arguments("ARG" "" "TITLE;PREFIX" "LIST" ${ARGN})
 	if(NOT ${ARG_TITLE} STREQUAL "")
@@ -8,8 +9,9 @@ function(Xi_printList)
 	endforeach()
 endfunction()
 
-function(Xi_addAllSubDirRaw path)
-	file(GLOB_RECURSE children LIST_DIRECTORIES true ${CMAKE_CURRENT_SOURCE_DIR}/${path}/*)
+# Add all folders under path containing cmake program to the building system 
+function(Xi_addAllSubDirGlob path)
+	file(GLOB_RECURSE children LIST_DIRECTORIES true ${path}/*)
 	set(dirs "")
 	foreach(item ${children})
 		if(IS_DIRECTORY ${item} AND EXISTS "${item}/CMakeLists.txt")
@@ -22,10 +24,17 @@ function(Xi_addAllSubDirRaw path)
 	endforeach()
 endfunction()
 
-function(Xi_addAllSubDir)
-	Xi_addAllSubDirRaw(src)
+# Relative path version
+function(Xi_addAllSubDirRel path)
+	Xi_addAllSubDirGlob(${CMAKE_CURRENT_SOURCE_DIR}/${path})
 endfunction()
 
+# Add all folders under 'root/src' containing cmake program to the building system
+function(Xi_addAllSubDir)
+	Xi_addAllSubDirGlob(${PROJECT_SOURCE_DIR}/src)
+endfunction()
+
+# Creat the source tree corresponding to the file system
 function(Xi_groupSrcs)
 	cmake_parse_arguments("ARG" "" "" "SOURCES" ${ARGN})
 	foreach(file ${ARG_SOURCES})
@@ -38,28 +47,35 @@ function(Xi_groupSrcs)
 	endforeach()
 endfunction()
 
+# Find all the files under a target and create the source tree
 function(Xi_groupAllSrcs)
 	file(GLOB_RECURSE srcs ${CMAKE_CURRENT_SOURCE_DIR}/*)
 	Xi_groupSrcs(SOURCES ${srcs})
 endfunction()
 
-function(Xi_getTargetNameRaw rst targetPath)
+# Get the name of the target from its path and folder name
+function(Xi_getTargetName rst targetPath)
 	file(RELATIVE_PATH targetRelPath "${PROJECT_SOURCE_DIR}/src" "${targetPath}")
 	string(REPLACE "/" "_" targetName "${PROJECT_NAME}/${targetRelPath}")
 	set(${rst} ${targetName} PARENT_SCOPE) 
 endfunction()
 
-function(Xi_getTargetName rst)
-	Xi_getTargetNameRaw(targetName ${CMAKE_CURRENT_SOURCE_DIR})
+# A specific version, call in current target folder
+function(Xi_getCurTargetName rst)
+	Xi_getTargetName(targetName ${CMAKE_CURRENT_SOURCE_DIR})
 	set(${rst} ${targetName} PARENT_SCOPE) 
 endfunction()
 
+# Create a target
 function(Xi_addTargetRaw)
-	cmake_parse_arguments("ARG" "" "MODE;QT" "SOURCES;LIBS_GENERAL;LIBS_DEBUG;LIBS_RELEASE" ${ARGN})
+	cmake_parse_arguments("ARG" "" "MODE" "SOURCES;LIBS" ${ARGN})
+
+	# target info
 	file(RELATIVE_PATH targetRelPath "${PROJECT_SOURCE_DIR}/src" "${CMAKE_CURRENT_SOURCE_DIR}/..")
 	set(folderPath "${PROJECT_NAME}/${targetRelPath}")
-	Xi_getTargetName(targetName)
+	Xi_getCurTargetName(targetName)
 	
+	# get all sources
 	list(LENGTH ARG_SOURCES sourceNum)
 	if(${sourceNum} EQUAL 0)
 		file(GLOB_RECURSE ARG_SOURCES ${CMAKE_CURRENT_SOURCE_DIR}/*)
@@ -71,6 +87,7 @@ function(Xi_addTargetRaw)
 		endif()
 	endif()
 	
+	# print info
 	message(STATUS "")
 	message(STATUS "---------- New Target ----------")
 	message(STATUS "- name: ${targetName}")
@@ -79,27 +96,11 @@ function(Xi_addTargetRaw)
 	Xi_printList(LIST ${ARG_SOURCES}
 		TITLE "- sources:"
 		PREFIX "    ")
-	
-	list(LENGTH ARG_LIBS_GENERAL generalLibNum)
-	list(LENGTH ARG_LIBS_DEBUG debugLibNum)
-	list(LENGTH ARG_LIBS_RELEASE releaseLibNum)
-	if(${debugLibNum} EQUAL 0 AND ${releaseLibNum} EQUAL 0)
-		if(NOT ${generalLibNum} EQUAL 0)
-		Xi_printList(LIST ${ARG_LIBS_GENERAL}
-			TITLE  "- lib:"
+	list(LENGTH ARG_LIBS libNum)
+	if(NOT ${libNum} EQUAL 0)
+		Xi_printList(LIST ${ARG_LIBS}
+			TITLE  "- libraries:"
 			PREFIX "    ")
-		endif()
-	else()
-		message(STATUS "- libs:")
-		Xi_printList(LIST ${ARG_LIBS_GENERAL}
-			TITLE  "  - general:"
-			PREFIX "      ")
-		Xi_printList(LIST ${ARG_LIBS_DEBUG}
-			TITLE  "  - debug:"
-			PREFIX "      ")
-		Xi_printList(LIST ${ARG_LIBS_RELEASE}
-			TITLE  "  - release:"
-			PREFIX "      ")
 	endif()
 	
 	# add target
@@ -121,22 +122,68 @@ function(Xi_addTargetRaw)
 	# folder
 	set_target_properties(${targetName} PROPERTIES FOLDER ${folderPath})
 	
-	foreach(lib ${ARG_LIBS_GENERAL})
-		target_link_libraries(${targetName} general ${lib})
-	endforeach()
-	foreach(lib ${ARG_LIBS_DEBUG})
-		target_link_libraries(${targetName} debug ${lib})
-	endforeach()
-	foreach(lib ${ARG_LIBS_RELEASE})
-		target_link_libraries(${targetName} optimized ${lib})
-	endforeach()
+	target_link_libraries(${targetName} PUBLIC ${ARG_LIBS})
+
 	install(TARGETS ${targetName}
 		RUNTIME DESTINATION "bin"
 		ARCHIVE DESTINATION "lib"
 		LIBRARY DESTINATION "lib")
 endfunction()
 
+# Specific version
 function(Xi_addTarget)
 	cmake_parse_arguments("ARG" "" "MODE" "LIBS" ${ARGN})
-	Xi_addTargetRaw(MODE ${ARG_MODE} LIBS_GENERAL ${ARG_LIBS})
+	Xi_addTargetRaw(MODE ${ARG_MODE} LIBS ${ARG_LIBS})
 endfunction()
+
+# Basic setting
+macro(Xi_projectInit)
+	# Add a character 'd' to the end of output filename under DEBUG mode
+	set(CMAKE_DEBUG_POSTFIX d)
+
+	# set C++ version
+	set(CMAKE_CXX_STANDARD 11)
+	set(CMAKE_CXX_STANDARD_REQUIRED True)
+
+	if(MSVC)
+		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /permissive-")
+	endif()
+
+	# set output folders (RUNTIME: exe, dll... | LIBRARY: lib | ARCHIVE: lib by dll)
+	set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_SOURCE_DIR}/bin")
+	set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG "${CMAKE_SOURCE_DIR}/bin")
+	set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE "${CMAKE_SOURCE_DIR}/bin")
+	set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_SOURCE_DIR}/lib")
+	set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY_DEBUG "${CMAKE_SOURCE_DIR}/lib")
+	set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE "${CMAKE_SOURCE_DIR}/lib")
+	set(CMAKE_LIBRARY_OUTPUT_DIRECTORY "${CMAKE_SOURCE_DIR}/lib")
+	set(CMAKE_LIBRARY_OUTPUT_DIRECTORY_DEBUG "${CMAKE_SOURCE_DIR}/lib")
+	set(CMAKE_LIBRARY_OUTPUT_DIRECTORY_RELEASE "${CMAKE_SOURCE_DIR}/lib")
+
+	# Open folder function of VS
+	set_property(GLOBAL PROPERTY USE_FOLDERS ON)
+
+	# set include folders
+	include_directories (
+		${PROJECT_SOURCE_DIR}/include
+		${PROJECT_SOURCE_DIR}/include/_deps
+		# $ENV{XXX_INCLUDE_PATH}
+		# ...
+	)
+
+	# set link folders
+	link_directories (
+		${PROJECT_SOURCE_DIR}/lib
+		# $ENV{XXX_LIB_PATH}
+		# ...
+	)
+endmacro()
+
+# call this if using Qt
+macro(Xi_QtInit)
+	set(CMAKE_INCLUDE_CURRENT_DIR ON)
+	set(CMAKE_AUTOMOC ON)
+	set(CMAKE_AUTOUIC ON)
+	set(CMAKE_AUTORCC ON)
+	find_package(Qt5Widgets REQUIRED)
+endmacro()
